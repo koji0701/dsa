982468698736594e5c6cb75eb09ada06956ff61b
Koji Wong <107135559+koji0701@users.noreply.github.com>
2026-06-13 19:17:30 -0500

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,38 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        starts = sorted([i.start for i in intervals])
+        ends = sorted([i.end for i in intervals])
+        
+        i = 0
+        j = 0 
+        curRooms = 0 
+        maxRooms = 0 
+
+        #last end no matter is after the last start
+        while i < len(intervals): 
+            if starts[i] < ends[j]: 
+                curRooms += 1
+                i += 1
+            else: 
+                curRooms -= 1
+                j +=1 
+
+            maxRooms = max(maxRooms, curRooms) 
+        
+        return maxRooms
+
+
+
+
+
+
+
+
