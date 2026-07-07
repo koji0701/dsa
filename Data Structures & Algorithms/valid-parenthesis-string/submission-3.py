98ff7b749b4022f6497009ff3e99d87a322590cc
Koji Wong <107135559+koji0701@users.noreply.github.com>
2026-07-07 13:26:39 -0500

    Add: valid-parenthesis-string - submission-3

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        minOpen = 0
+        maxOpen = 0
+
+        for v in s: 
+
+            if v == "*": 
+                minOpen = max(0, minOpen - 1)
+                maxOpen += 1
+            elif v == "(": 
+                minOpen += 1
+                maxOpen += 1
+            else: 
+                minOpen -= 1
+                maxOpen -= 1
+
+            if maxOpen < 0: 
+                return False 
+
+        
+        
+        print(minOpen, maxOpen)
+        return minOpen <= 0
+
+
+
+
